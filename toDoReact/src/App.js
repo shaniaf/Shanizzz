1766fbea7f01910d862d88d1fb5cde85966ba761
Shani <safrimi@paypal.com>
2020-07-19 08:06:35 +0300

    added todo folders

---
+++ toDoReact/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos.js';
+
+class App extends Component {
+  render(){
+
+  return (
+    <div className="App">
+    <Todos/>
+    </div>
+  );
+}
+}
+export default App;
